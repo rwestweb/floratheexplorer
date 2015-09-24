ba75efb4597b82a2207d0b539bbec51fb06e8d61
Rebecca West <rebecca@rebeccawest.net>
2015-09-23 19:44:38 -0600

    test 2

---
+++ www/floraForm/views/nathanExamples.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rebeccawest
+ * Date: 9/23/15
+ * Time: 6:13 PM
+ */
+
+// TTD - remove baggage in head to solve problems to move forward on othe rproblems
+// We will be doing unit testing; integration testing is dirtier (how does the app work, automate the browser, etc)
+
+
+reuire(!functions.php)
